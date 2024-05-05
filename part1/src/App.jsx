3bea468ded620b72dcb86897caae87c10796c6b6
Rigoberto Antonio Maximiliano Martínez Quintana <rigomartinez@udec.cl>
2024-05-05 00:00:00 -0400

    (atlas)edit:App(comp course refac)

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const Header;
+const Header = ({ course }) => <h1>{course}</h1>;
 const Content;
 const Total;
 
@@ -16,7 +16,7 @@ const App = () => {
 
   return (
     <div>
-      <h1>{course}</h1>
+      <Header course={course} />
       <p>
         {part1} {exercises1}
       </p>
