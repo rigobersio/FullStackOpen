339b2b5842bf8452a383aab48b86958a70defb19
Rigoberto Antonio Maximiliano Martínez Quintana <rigomartinez@udec.cl>
2024-05-13 00:00:00 -0400

    (atlas)edit:App(comp Content refac)

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,7 +1,21 @@
 import React from 'react'
 
 const Header = ({ course }) => <h1>{course}</h1>;
-const Content;
+const Content = ({ part1, part2, part3, exercises1, exercises2, exercises3 }) => {
+  return (
+    <>
+      <p>
+        {part1} {exercises1}
+      </p>
+      <p>
+        {part2} {exercises2}
+      </p>
+      <p>
+        {part3} {exercises3}
+      </p>
+    </>
+  )
+};
 const Total;
 
 
@@ -17,15 +31,7 @@ const App = () => {
   return (
     <div>
       <Header course={course} />
-      <p>
-        {part1} {exercises1}
-      </p>
-      <p>
-        {part2} {exercises2}
-      </p>
-      <p>
-        {part3} {exercises3}
-      </p>
+      <Content part1={part1} part2={part2} part3={part3} exercises1={exercises1} exercises2={exercises2} exercises3={exercises3} />
       <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
     </div>
   )
