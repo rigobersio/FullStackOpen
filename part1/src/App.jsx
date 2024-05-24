abce0ee61c03a4ef953054731c94d6828286cbff
Rigoberto Antonio Maximiliano Martínez Quintana <rigomartinez@udec.cl>
2024-05-24 00:00:00 -0400

    (atlas)edit:App(comp Total refac)

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -16,7 +16,7 @@ const Content = ({ part1, part2, part3, exercises1, exercises2, exercises3 }) =>
     </>
   )
 };
-const Total;
+const Total = ({ exercises1, exercises2, exercises3 }) => <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>;
 
 
 const App = () => {
@@ -32,7 +32,7 @@ const App = () => {
     <div>
       <Header course={course} />
       <Content part1={part1} part2={part2} part3={part3} exercises1={exercises1} exercises2={exercises2} exercises3={exercises3} />
-      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+      <total exercises1={exercises1} exercises2={exercises2} exercises3={exercises3} />
     </div>
   )
 }
