73b0397b6bc5258dc6d6a50d45ae33c8d3e1fb50
Rigoberto Antonio Maximiliano Martínez Quintana <rigomartinez@udec.cl>
2024-08-05 00:00:00 -0400

    (atlas)new:App, Anecdotes()

--- part1/anecdotes/src/App.jsx
+++ part1/anecdotes/src/App.jsx
@@ -1,16 +1,19 @@
 import React, { useState } from 'react';
+import Anecdotes from './components/Anecdotes';
+
 
 
 
 
 const App = () => {
   
- 
+  const [selected, setSelected] = useState(0)
   
 
   return (
     <div>
-      
+      <h1>Anecdote of the day</h1>
+      <Anecdotes selected={selected} setSelected={setSelected} />
     </div>
   )
 }
