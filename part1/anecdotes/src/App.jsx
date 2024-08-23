ddfd76ce987aa99bbd2c9997c69e7ed321c22c8c
Rigoberto Antonio Maximiliano Martínez Quintana <rigomartinez@udec.cl>
2024-08-23 00:00:00 -0400

    (atlas)( ok Anecdotes)

--- part1/anecdotes/src/App.jsx
+++ part1/anecdotes/src/App.jsx
@@ -11,17 +11,38 @@ const objVotes = {
   6: 0,
   7: 0
 };
+
+const arrAnecdotes = [
+  'If it hurts, do it more often.',
+  'Adding manpower to a late software project makes it later!',
+  'The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+  'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+  'Premature optimization is the root of all evil.',
+  'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+  'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.',
+  'The only way to go fast, is to go well.',
+];
 const App = () => {
   
   const [selected, setSelected] = useState(0);
   const [allVotes, setAllVotes] = useState(objVotes);
-  
+
+
+
+const keyGanadora = Object.keys(allVotes).reduce((maxKey, index) => {
+  return allVotes[maxKey] > allVotes[index] ? maxKey : index;
+}, 0); // cada vez que allVotes cambia, KeyGanadora se actualizará porque el componente se renderiza otra vez.
 
   return (
     <div>
       <h2>Anecdote of the day</h2>
-      <h2>has {allVotes[selected]} votes</h2>
-      <Anecdotes selected={selected} setSelected={setSelected} allVotes={allVotes} setAllVotes={setAllVotes} />
+      <Anecdotes arrAnecdotes={arrAnecdotes} selected={selected} setSelected={setSelected} allVotes={allVotes} setAllVotes={setAllVotes} />
+      <p>has {allVotes[selected]} votes</p>
+      <br />
+      <h2>Anecdote with most votes</h2>
+      <p>{arrAnecdotes[keyGanadora]}</p>
+      <p>has {allVotes[keyGanadora]} votes</p>
+      
     </div>
   )
 }
